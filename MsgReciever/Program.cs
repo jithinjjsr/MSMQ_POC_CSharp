c53a4e7361d33aab383c2f3b816bfc588be1d1bd
Jithin Raveendran <Jithin Raveendran>
2025-07-26 15:39:48 +0100

    Add project files.

---
+++ MsgReciever/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Messaging;
+
+class Receiver
+{
+    static void Main()
+    {
+        string queuePath = @".\Private$\MyTestQueue";
+
+        using (MessageQueue mq = new MessageQueue(queuePath))
+        {
+            mq.Formatter = new XmlMessageFormatter(new Type[] { typeof(string) });
+            mq.ReceiveCompleted += (sender, e) =>
+            {
+                try
+                {
+                    var msg = mq.EndReceive(e.AsyncResult);
+                    Console.WriteLine("Received: " + msg.Body.ToString());
+                }
+                catch (MessageQueueException ex)
+                {
+                    Console.WriteLine("Error: " + ex.Message);
+                }
+                // Continue listening for the next message
+                mq.BeginReceive();
+            };
+
+            // Start listening for messages
+            mq.BeginReceive();
+
+            Console.WriteLine("Listening for messages. Press Enter to exit.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MsgSender/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Messaging;
+
+class Program
+{
+    static void Main()
+    {
+        string queuePath = @".\Private$\MyTestQueue";
+
+        if (!MessageQueue.Exists(queuePath))
+        {
+            MessageQueue.Create(queuePath);
+            Console.WriteLine("Queue created.");
+        }
+        string userInput = "N";
+        do
+        {
+            Console.WriteLine("Enter the message to sent : ");
+            string msg = Console.ReadLine();
+            using (MessageQueue mq = new MessageQueue(queuePath))
+            {
+                mq.Send(msg);
+                Console.WriteLine("Message sent to MSMQ.");
+            }
+
+            Console.WriteLine("Do you want to send enother message? (y/n) : ");
+            userInput = Console.ReadLine();
+        } while (userInput.ToLower().Trim() == "y" );
+        
+    }
+}
